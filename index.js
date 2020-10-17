a3b458bd4851db69b33cdbd6d86d067d83a179ad
Naufal Hanif <nahanifg@gmail.com>
2020-10-17 10:41:12 +0700

    add route users

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
 
@@ -6,3 +7,5 @@ const port = process.env.PORT || 3000
 app.get('/', (request, response) => response.json({ info: 'Node.js, Express, and Postgres API' }))
 
 app.listen(port, () => console.log(`App running on port ${port}.`))
+
+app.get('/users', (request, response) => response.status(200).json({ users: [{user: "halo"}] }))
